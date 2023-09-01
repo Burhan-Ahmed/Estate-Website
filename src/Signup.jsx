c5166972f5fac3259fdf0a789008aa58a1e993f9
Muhammad Burhan Ahmed <burhan.ahmed60090@gmail.com>
2023-09-01 18:00:50 +0500

    Updates

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,23 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import './styles/Signup.css'
+
 
 export default function Signin() {
   return (
-    <div className="px-10 py-20 my-48 w-max h-max mx-auto border border-black rounded-xl">
-      <div className="pt-28 pb-16 text-center">
+    <div className="container">
+      <div className="icon pb-16 text-center">
         <FontAwesomeIcon icon={faUser} size="3x" />
       </div>
-      <div className="px-28 p-24 text-center">
-        <h1 className="mb-5 font-semibold text-5xl">
+      <div>
+        <h1>
           Islamabad Real Estates
         </h1>
-        <p className="mb-12 font-mono text-md">
+        <p>
           Invest in Future
         </p>
-        <form className="flex flex-col space-y-2">
-          <input className="py-2 ps-3 rounded focus:outline-none" placeholder="Enter Email" type="text" required />
-          <input className="py-2 ps-3 rounded focus:outline-none" placeholder="Enter your Password" type="password" required />
-          <button type="submit" className="py-1 px-3 hover:text-white hover:bg-black ease-in-out border border-black rounded">
+        <form className="inputForm space-y-2">
+          <input placeholder="Enter Email" type="text" required />
+          <input placeholder="Enter your Password" type="password" required />
+          <button type="submit" className="py-1 px-3">
             Login
           </button>
         </form>
